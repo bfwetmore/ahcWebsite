e6b671c2d260a697c5d608303e4bc2b96dd93e68
Christina Harding <christinaharding@Christinas-MacBook-Air.local>
2023-11-21 08:26:19 -0500

    WIP

--- src/scripts/custom.js
+++ src/scripts/custom.js
@@ -18,32 +18,52 @@
     })
   })()
 
-  export default {
-    async fetch(request) {
-      send_request = new Request('https://api.mailchannels.net/tx/v1/send', {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
+  addEventListener("fetch", event => {
+    event.respondWith(handleRequest(event.request))
+})
+ 
+async function handleRequest(request) {
+    let content = "";
+    for( var i of request.headers.entries() ) {
+        content += i[0] + ": " + i[1] + "\n";
+    }
+    let send_request = new Request("https://api.mailchannels.net/tx/v1/send", {
+        "method": "POST",
+        "headers": {
+            "content-type": "application/json",
         },
-        body: JSON.stringify({
-          personalizations: [
-            {
-              to: [{ email: 'bfwetmore@gmail.com', name: 'Test Recipient' }],
+        "body": JSON.stringify({
+            "personalizations": [
+                { "to": [ {"email": "recipient@example.com",
+                        "name": "Test Recipient"}]}
+            ],
+            "from": {
+                "email": "sender@example.com",
+                "name": "Test Sender",
             },
-          ],
-          from: {
-            email: 'sender@example.com',
-            name: 'Workers - MailChannels integration',
-          },
-          subject: 'Look! No servers',
-          content: [
-            {
-              type: 'text/plain',
-              value: 'And no email service accounts and all for free too!',
-            },
-          ],
+            "subject": "Test Subject",
+            "content": [{
+                "type": "text/plain",
+                "value": "Test message content\n\n" + content,
+            }],
         }),
-      })
-    },
-  }
-  
+    });
+ 
+    let respContent = "";
+    // only send the mail on "POST", to avoid spiders, etc.
+    if( request.method == "POST" ) {
+        const resp = await fetch(send_request);
+        const respText = await resp.text();
+ 
+        respContent = resp.status + " " + resp.statusText + "\n\n" + respText;
+ 
+    }
+ 
+    let htmlContent = "<html><head></head><body><pre>" +
+        '</pre><p>Click to send message: <form method="post"><input type="submit" value="Send"/></form></p>' +
+        "<pre>" + respContent + "</pre>" +
+        "</body></html>";
+    return new Response(htmlContent, {
+        headers: { "content-type": "text/html" },
+    })
+}
