e6e5e8ddf6e18158018062d36084c7c8dc6765a4
Christina Harding <christinaharding@Christinas-MacBook-Air.local>
2023-11-23 11:42:29 -0500

    WIP

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,18 +1,34 @@
-import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+// import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
-export const onRequest: PagesFunction = mailChannelsPlugin({
+// export const onRequest: PagesFunction = mailChannelsPlugin({
+//   personalizations: [
+//     {
+//       to: [{ name: "AHC", email: "bfwetmore@gmail.com" }],
+//     },
+//   ],
+//   from: {
+//     name: "New Lead",
+//     email: "leads@approvedheatingandcooling.com",
+//   },
+//   respondWith: () => {
+//     return new Response(
+//       `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
+//     );
+//   },
+// });
+
+import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+
+export const onRequest = mailchannelsPlugin({
   personalizations: [
     {
       to: [{ name: "AHC", email: "bfwetmore@gmail.com" }],
     },
   ],
-  from: {
-    name: "New Lead",
-    email: "leads@approvedheatingandcooling.com",
-  },
-  respondWith: () => {
-    return new Response(
-      `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
-    );
-  },
+  from: { name: "Lead", email: "lead@approvedheatingandcooling.com" },
+  respondWith: () =>
+    new Response(null, {
+      status: 302,
+      headers: { Location: "/" },
+    }),
 });
